8be606479c8dfdda5bdcf072723aebdbb85906e5
kingpoogie <roflbob@gmail.com>
2025-05-07 23:19:58 -0400

    connect.py and movie.py updates
    Functions search, format, and movie.py are more comprehensive.

--- src/connect.py
+++ src/connect.py
@@ -1,4 +1,5 @@
 import requests
+from movie import Movie
 
 #This class connects to the OMDB API and collects information about the movies or errors
 class Connect():
@@ -6,25 +7,36 @@ class Connect():
         self.URL = URL
         self.API_KEY = API_KEY
 
-    #connect to the api and search for a movie. 
+    #connect to the api and search for a movie. Returns an array with either movie objects or an error message
     def search(self, title):
         params = {"apikey":self.API_KEY, "s":title}
         response = requests.get(self.URL, params = params)
         #check if successful
         if response.status_code == 200:
             data = response.json()
-            print(data)
+            #print(data)
+            self.format(data)
         else:
-            #unsuccessful connection
+            #unsuccessful connection. Set error message and return it in a list
+            error_message = f"Connection unsuccessful. Response code {response.status_code}"
             print(f"connection unsuccessful. {response.status_code}")
-        
-        
-
+            return [error_message]
 
     #if the connection was successful, format the json query into usable data
-    def format(self):
-        pass
+    def format(self, data):
+        movie_list = []
+        print(data)
+        #data should be a dictionary
+        #check if "Search" key exists
+        if "Search" in data:
+            #in the value is a list of dictionaries. Each dictionary has the movie information
+            #in the following keys: Title, Year, imdbID, Type, Poster (url)
+            for movie in data["Search"]:
+                search_result_movie = Movie(movie)
+                movie_list.append(search_result_movie)
+            for each in movie_list:
+                print(each.get_title())
 
 
-connection = Connect("http://www.omdbapi.com/", "CODE HERE")
+connection = Connect("http://www.omdbapi.com/", "9cda324")
 connection.search("Back to the Future")
--- src/movie.py
+++ src/movie.py
@@ -1,10 +1,12 @@
 class Movie():
     #movie is a dictionary containing information about the movie
     #{"title":"jaws","year":1979,"id"="1235td"}
-    def __init__(self, movie):
-        self._title = movie["title"]
-        self._year = movie["year"]
-        self._id = movie["id"]
+    def __init__(self, movie = {"Title":"N/A", "Year":"N/A", "imdbID":"N/A", "Type":"N/A", "Poster":"N/A"}):
+        self._title = movie["Title"]
+        self._year = movie["Year"]
+        self._id = movie["imdbID"]
+        self._type = movie["Type"]
+        self._thumbnail_url = movie["Poster"]
     
     def get_title(self):
         return self._title
