f8d26d4fd1a7c65dee69ff37063de156766bf601
kingpoogie <bmadeley@getgoldn.com>
2025-05-11 23:07:21 -0400

    Added functionality to the Lookup button. Next up: need to connect to the API and lookup specific movie information

--- src/movielogger.py
+++ src/movielogger.py
@@ -16,6 +16,8 @@ class MovieLogger():
         self.root.title("Movie Logger")
         self.root.geometry("800x800")
 
+        self._OMDB_URL = "http://www.omdbapi.com/"
+
         #get default background color
         bg_color = root.cget("background")
         rgb = root.winfo_rgb(bg_color)
@@ -41,11 +43,6 @@ class MovieLogger():
         self.menubar.add_cascade(label="Review", menu=self.review_menu)
         self.review_menu.add_command(label="Add/Update Review")
         self.review_menu.add_command(label="Read Review")
-
-        #Movie
-        self.movie_menu = tk.Menu(self.menubar, tearoff=0)
-        self.menubar.add_cascade(label="Movie", menu=self.movie_menu)
-        self.movie_menu.add_command(label="Lookup")
     
         #create the frames
         self.watchlist_frame = tk.Frame(self.root)
@@ -75,7 +72,6 @@ class MovieLogger():
         self.search_results_canvas.grid(row=2, column=0, columnspan=3, sticky="nsew")
         self.watchlist_canvas = tk.Canvas(self.watchlist_frame, bg=bg_color, highlightbackground="blue")
         self.watchlist_canvas.grid(row=1, column=0, columnspan=3, sticky="nsew")
-    
 
         #create scrollbar within the search_results canvas
         self.search_scrollbar = ttk.Scrollbar(self.root, orient="vertical", command=self.search_results_canvas.yview)
@@ -92,6 +88,7 @@ class MovieLogger():
         self.movies_container.grid_columnconfigure(0, weight=1) #column 0 expands
 
         self.search_results_window = self.search_results_canvas.create_window((0,0), window=self.movies_container, anchor="nw")
+
 
         #add event bindings
         #this one sets the width of the search_results_window to be the width of the search_results_canvas
@@ -146,6 +143,7 @@ class MovieLogger():
 
         for movie in self.movies_query:
             single_movie_frame = tk.Frame(self.movies_container)
+            single_movie_frame.movie = movie
             if not (isinstance(movie, Movie)):
                 raise TypeError("movie must be a Movie object")
 
@@ -156,7 +154,7 @@ class MovieLogger():
             thumbnail_label = tk.Label(single_movie_frame, text="URL")
 
             #make the buttons
-            lookup_button = tk.Button(single_movie_frame, text="Lookup")
+            lookup_button = tk.Button(single_movie_frame, text="Lookup", command=lambda m=single_movie_frame.movie: self.lookup_movie(m))
             watchlist_button = tk.Button(single_movie_frame, text="Watchlist")
             favorites_button = tk.Button(single_movie_frame, text="Favorite")
             review_button = tk.Button(single_movie_frame, text="Review")
@@ -211,7 +209,36 @@ class MovieLogger():
             self.watchlist_scrollbar.grid(row=5, column=0, columnspan=3, sticky="ew")
             self.root.rowconfigure(2, weight=1)
             self.root.rowconfigure(4, weight=1)
+    
+    #adds the movie to the watchlist
+    #takes a movie object
+    def add_to_watchlist(self, movie):
+        return
 
+    #opens a new top level window that has more information about the movie such as a bigger thumbnail, actors, plot, etc
+    #takes a movie object
+    def lookup_movie(self, movie=None):
+        if movie is None:
+            print("error, lookup button clicked and movie is None")
+            return
+        #find the name of the movie frame that was clicked
+        lookup_window = tk.Toplevel(self.root)
+
+        #from here, connect to the API and search for the ttid. Then, gather the extra information and display it in a window
+        movie_lookup = Connect(self._OMDB_URL)
+        lookup_window.title(movie.get_title())
+
+
+        #create the frame
+        tk.Label(lookup_window)
+
+    #adds the movie to the favorites list
+    def add_to_favorites(self, movie):
+        return
+    
+    #open the personal review for the specified movie
+    def open_reviews(self, movie):
+        return
 
 
 generateUI()
